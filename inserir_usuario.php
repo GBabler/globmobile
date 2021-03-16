59929259cc2735545151040996fcb5add6bd4b8d
GBabler <gabriel.babler1@hotmail.com>
2021-03-16 20:16:01 -0300

    update

--- inserir_usuario.php
+++ inserir_usuario.php
@@ -22,31 +22,41 @@ if (isset($_POST['nome']) && empty($_POST['nome']) == false) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-BR">
 <head>
-	<meta charset="utf-8">
-	<meta name = "viewport" content="widht=device-width, inicial-scale=1.0">
-	<div style="text-align: right;">
-		<?php echo "$_SESSION[nome]";?> 
-		<!-- mensagem de bem vindo com o nome do usuário -->
-	</div>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inserir Usuario</title>
+
+	<link rel="stylesheet" type="text/css" href="./css/inserir_usuario.css">
 </head>
+<body> 
 
-<body>
+	<header class="header">
+			<h1>Registro de Usuario</h1>	
+	</header>
 	<form method="POST">
+		<div class="color-form">
+			<div class="centralizar">
 
-		<div>
-		<label>Nome do usuario: </label><br>
-		<input type="text" name="nome"/> </br>
+				<div class="input-label">
+				<label>Nome: </label><br>
+				<input type="text" name="nome"/><br><br>
+							
+				<label>Email: </label><br>
+				<input type="text" name="email"> <br><br>
+
+				<label>N° Celular</label>
+				<input type="text" name="celular">
+				</div>
 			
-		<label>Email: </label><br>
-		<input type="text" name="email"> </br>
+			<div class="div-button">
+			<button type="submit" class="button">Inserir</button>
+			<a href="lista_usuario.php"><button type="button" class="button">Voltar</button><br><p></a>
+		</div>
+		</div>
 		</div>	
-		
-		<br>
-		<button type="submit">Inserir</button>
-		<a href="lista_usuario.php"><button type="button">Voltar</button><br><p></a>
 	</form>
 
 </body>
